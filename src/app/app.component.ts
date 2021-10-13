92fd03c11f3418d3fde4b3868075e5fd4f80c033
Toan Dang <19522357@gm.uit.edu.vn>
2021-10-13 22:20:19 +0700

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyBubU9lAPMYW18c-BxSNXMSk_xKf5DHRhM',
+  databaseURL: 'https://angularchat-4b198-default-rtdb.firebaseio.com',
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angularChatApp';
+  title = 'angular-chat';
+
+  constructor() {
+    firebase.initializeApp(config);
+  }
 }
